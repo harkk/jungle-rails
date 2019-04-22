ed1506653a9e626556390b9e53ca756932070057
Harkamal Khabra <khabra07@gmail.com>
2019-04-22 15:38:49 +0000

    Rspec setup

---
+++ test/mailers/previews/order_mailer_preview.rb
@@ -0,0 +1,10 @@
+class OrderMailerPreview < ActionMailer::Preview
+  
+  default from: "no-reply@jungle.com"
+
+  def order_confirm_email(order)
+    @order = order
+    mail(to: "@order.email", subject: 'Thanks for your order at Jungle.com')
+  end
+
+  end
